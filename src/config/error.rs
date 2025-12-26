79206a88d19f8ff5a7f3b1a8f2b3ec10c3796fe9
Manny <mnmbrane@gmail.com>
2025-12-26 11:06:41 -0500

    Config refactor

--- src/config/error.rs
+++ src/config/error.rs
@@ -1,10 +1,24 @@
 #[derive(Debug)]
 pub enum ConfigError {
     Io(std::io::Error),
+    TomlDe(toml::de::Error),
+    TomlSer(toml::ser::Error),
 }
 
 impl From<std::io::Error> for ConfigError {
     fn from(value: std::io::Error) -> Self {
         ConfigError::Io(value)
+    }
+}
+
+impl From<toml::de::Error> for ConfigError {
+    fn from(value: toml::de::Error) -> Self {
+        ConfigError::TomlDe(value)
+    }
+}
+
+impl From<toml::ser::Error> for ConfigError {
+    fn from(value: toml::ser::Error) -> Self {
+        ConfigError::TomlSer(value)
     }
 }
--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,23 +1,9 @@
+pub mod config_util;
 pub mod error;
 pub mod port_config;
-
-use std::{collections::HashMap, fs, path::PathBuf};
-
+pub use config_util::ConfigUtil;
 pub use error::ConfigError;
 pub use port_config::PortConfig;
+use std::{collections::HashMap, fs, path::Path};
 
-pub type PortName = String;
-
-pub struct Config;
-
-impl Config {
-    fn load<T: DeserializeOwned(path: &PathBuf) -> Result<String, ConfigError> {
-        let content = fs::read_to_string(path)?;
-        Ok(content)
-    }
-
-    fn save(path: &PathBuf, contents: String) -> Result<(), ConfigError> {
-        std::fs::write(path, contents)?;
-        Ok(())
-    }
-}
+use serde::de::DeserializeOwned;
