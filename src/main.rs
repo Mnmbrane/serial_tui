cd752d0cf53c9f0c66d5cb1575851bc7c794bb9b
Manny <mnmbrane@gmail.com>
2026-02-07 11:52:53 -0500

    Move app to main, it's simple enough

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 //! SerialTUI entry point.
 
-mod app;
 mod config;
 mod error;
 mod logger;
@@ -9,14 +8,31 @@ mod serial;
 mod types;
 mod ui;
 
-use anyhow::Result;
+use std::sync::Arc;
 
-use crate::app::App;
+use anyhow::Result;
+use tokio::sync::mpsc;
+
+use crate::{notify::Notify, serial::hub::SerialHub, ui::Ui};
 
 fn main() -> Result<()> {
     let rt = tokio::runtime::Runtime::new()?;
     let _guard = rt.enter();
-    let mut app = App::new();
-    app.run()?;
+
+    let config_path = config::ensure_config();
+
+    let (notify_tx, notify_rx) = mpsc::unbounded_channel::<Notify>();
+    let (log_tx, log_rx) = mpsc::unbounded_channel();
+
+    let (mut hub, port_recv_rx) = SerialHub::new(notify_tx, log_tx);
+    hub.load_config(config_path)
+        .unwrap_or_else(|e| eprintln!("{e}"));
+
+    tokio::spawn(logger::run(log_rx));
+
+    let hub = Arc::new(hub);
+    let mut ui = Ui::new(hub, port_recv_rx, notify_rx);
+    ui.run()?;
+
     Ok(())
 }
